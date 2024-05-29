acb897468dec5d3b4f029dc7be18021a4e53b918
Robin Sanchez <robin.sanchez@ambacia.lan>
2024-05-29 17:01:13 +0200

    /preferences Premium chart

--- src/Controller/PreferencesController.php
+++ src/Controller/PreferencesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\ChartBuilders\FreemiumChart;
+use App\ChartBuilders\PremiumChart;
 use App\Form\DeleteAccountType;
 use App\Form\PreferencesType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,7 +12,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
-use Symfony\UX\Chartjs\Model\Chart;
 
 class PreferencesController extends AbstractController
 {
@@ -20,91 +21,13 @@ class PreferencesController extends AbstractController
         $preferencesForm = $this->createForm(PreferencesType::class);
         $deleteAccountForm = $this->createForm(DeleteAccountType::class);
 
-        $chartViewProfil = $chartBuilder->createChart(Chart::TYPE_BAR);
-        $chartViewProfil->setData([
-            'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-            'datasets' => [
-                [
-                    'label' => 'Nombre de vues de mon profil',
-                    'backgroundColor' => 'blue',
-                    'borderColor' => 'rgb(255, 99, 132)',
-                    'data' => [0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550],
-                ],
-            ],
-        ]);
-
-        $chartViewProfil->setOptions([
-            'scales' => [
-                'y' => [
-                    'suggestedMin' => 0,
-                    'suggestedMax' => 600,
-                    'ticks' => [
-                        'stepSize' => 50,
-                    ],
-                ],
-            ],
-            'responsive' => true,
-        ]);
-
-        $chartViewInvitations = $chartBuilder->createChart(Chart::TYPE_BAR);
-        $chartViewInvitations->setData([
-            'labels' => ['Reçues'],
-            'datasets' => [
-                [
-                    'label' => 'Nombre d\'évènements',
-                    'backgroundColor' => 'black',
-                    'borderColor' => 'rgb(255, 99, 132)',
-                    'data' => [50],
-                ],
-            ],
-        ]);
-
-        $chartViewInvitations->setOptions([
-            'scales' => [
-                'y' => [
-                    'suggestedMin' => 0,
-                    'suggestedMax' => 150,
-                    'ticks' => [
-                        'stepSize' => 50,
-                    ],
-                ],
-                'x' => [
-                    'barPercentage' => 0.5,
-                    'categoryPercentage' => 0.5,
-                ],
-            ],
-            'responsive' => true,
-        ]);
-
-        $chartViewEvents = $chartBuilder->createChart(Chart::TYPE_BAR);
-        $chartViewEvents->setData([
-            'labels' => ['Suivis'],
-            'datasets' => [
-                [
-                    'label' => 'Nombre d\'évènements',
-                    'backgroundColor' => 'black',
-                    'borderColor' => 'rgb(255, 99, 132)',
-                    'data' => [30],
-                ],
-            ],
-        ]);
-
-        $chartViewEvents->setOptions([
-            'scales' => [
-                'y' => [
-                    'suggestedMin' => 0,
-                    'suggestedMax' => 150,
-                    'ticks' => [
-                        'stepSize' => 50,
-                    ],
-                ],
-                'x' => [
-                    'barPercentage' => 0.5,
-                    'categoryPercentage' => 0.5,
-                ],
-            ],
-            'responsive' => true,
-        ]);
+        $chartsFreemium = FreemiumChart::buildChart($chartBuilder);
+        $chartsPremium = PremiumChart::buildChart($chartBuilder);
+        $chartViewProfil = $chartsFreemium['profile'];
+        $chartViewInvitations = $chartsFreemium['invitations'];
+        $chartViewEvents = $chartsFreemium['events'];
+        $chartViewLike = $chartsPremium['like'];
+        $chartViewQuote = $chartsPremium['quotation'];
 
         return $this->render('preferences/index.html.twig', [
             'controller_name' => 'PreferencesController',
@@ -113,6 +36,8 @@ class PreferencesController extends AbstractController
             'chart1' => $chartViewProfil,
             'chart2' => $chartViewInvitations,
             'chart3' => $chartViewEvents,
+            'chart4' => $chartViewLike,
+            'chart5' => $chartViewQuote,
         ]);
     }
 }
