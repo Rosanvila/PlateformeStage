ba845c60ce65a6ad66a6a58ff555c6dd5494ebaa
louis_drouet <louis.drouet@ambacia.net>
2024-06-10 10:36:35 +0200

    correction merge

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -3,15 +3,22 @@
 namespace App\Controller;
 
 use App\Entity\Company;
+use App\Repository\CompanyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class CompanyController extends AbstractController
 {
-    #[Route('/company/{id}', name: 'app_company')]
-    public function index(Company $company): Response
+    #[Route('/{_locale}/company/{id}', name: 'app_company')]
+    public function index(int $id, CompanyRepository $companyRepository): Response
     {
+        $company = $companyRepository->find($id);
+
+        if (!$company) {
+            throw $this->createNotFoundException('The company does not exist');
+        }
+
         return $this->render('company/index.html.twig', [
             'company' => $company,
         ]);
