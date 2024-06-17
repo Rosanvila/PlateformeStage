adadfc85f83c4fb6dbf5750005d60f0ed40766fb
kevinmarie <kevin.marie@ambacia.net>
2024-06-17 16:36:28 +0200

    Reprise de la prévisu de la photo sur register et divers bugs

--- assets/js/register.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("registration_form_picture").onchange = function() {
-        const reader = new FileReader();
-            reader.onload = function() {
-                document.getElementById("picturePreview").src = reader.result;
-            };
-            reader.readAsDataURL(this.files[0]);
-        };
-});
