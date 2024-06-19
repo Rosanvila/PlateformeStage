35cedc94fc30c5000fc5c4b9d6dee29b8fe851f4
Robin Sanchez <robin.sanchez@ambacia.lan>
2024-06-19 14:20:09 +0200

    added width 100% to dropdown menu

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,8 +1,8 @@
 import {Dropdown} from 'bootstrap';
 
-window.addEventListener('DOMContentLoaded', (event) => {
+/*window.addEventListener('DOMContentLoaded', (event) => {
     const dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
     const dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
         return new Dropdown(dropdownToggleEl)
     })
-});
+});*/
